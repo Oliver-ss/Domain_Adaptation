28ebb8128b903fe583770b0c017529e407372d29
Oliver_ss <yuansongwx@outlook.com>
2019-10-25 19:11:55 -0400

    combine test_bn.py and test.py

--- configs/xh.deeplab.mobilenet.shanghai/test.py
+++ configs/xh.deeplab.mobilenet.shanghai/test.py
@@ -9,23 +9,35 @@ from data import spacenet
 from common import config
 from torch.utils.data import DataLoader
 import numpy as np
-
+import cv2
 
 class Test:
-    def __init__(self, model_path, config, cuda=False):
+    def __init__(self, model_path,config, bn, save_path, save_batch, cuda=False):
+        self.bn = bn
         self.target=config.all_dataset
         self.target.remove(config.dataset)
         # load source domain
         self.source_set = spacenet.Spacenet(city=config.dataset, split='test', img_root=config.img_root)
         self.source_loader = DataLoader(self.source_set, batch_size=16, shuffle=False, num_workers=2)
 
+        self.save_path = save_path
+        self.save_batch = save_batch
+
         self.target_set = []
         self.target_loader = []
+
+        self.target_trainset = []
+        self.target_trainloader = []
+
         # load other domains
         for city in self.target:
-            tmp = spacenet.Spacenet(city=city, split='test', img_root=config.img_root)
-            self.target_set.append(tmp)
-            self.target_loader.append(DataLoader(tmp, batch_size=16, shuffle=False, num_workers=2))
+            test = spacenet.Spacenet(city=city, split='test', img_root=config.img_root)
+            self.target_set.append(test)
+            self.target_loader.append(DataLoader(test, batch_size=16, shuffle=False, num_workers=2))
+            if bn:
+                train = spacenet.Spacenet(city=city, split='train', img_root=config.img_root)
+                self.target_trainset.append(train)
+                self.target_trainloader.append(DataLoader(train, batch_size=16, shuffle=False, num_workers=2))
 
         self.model = DeepLab(num_classes=2,
                 backbone=config.backbone,
@@ -43,10 +55,29 @@ class Test:
         if cuda:
             self.model = self.model.cuda()
 
-    def get_performance(self, dataloader):
+    def get_performance(self, dataloader, trainloader, city):
+        # change mean and var of bn to adapt to the target domain
+        if self.bn and city != config.dataset:
+            self.model.train()
+            for sample in trainloader:
+                image, target = sample['image'], sample['label']
+                if self.cuda:
+                    image, target = image.cuda(), target.cuda()
+                with torch.no_grad():
+                    output = self.model(image)
+
+        batch = self.save_batch
         self.model.eval()
         self.evaluator.reset()
         tbar = tqdm(dataloader, desc='\r')
+
+        # save in different directories
+        if self.bn:
+            save_path = os.path.join(self.save_path, city + '_bn')
+        else:
+            save_path = os.path.join(self.save_path, city)
+
+       # evaluate on the test dataset
         for i, sample in enumerate(tbar):
             image, target = sample['image'], sample['label']
             if self.cuda:
@@ -59,17 +90,30 @@ class Test:
             # Add batch sample into evaluator
             self.evaluator.add_batch(target, pred)
 
-        # Fast test during the training
+            # save pictures
+            if batch > 0:
+                if not os.path.exists(self.save_path):
+                    os.mkdir(self.save_path)
+                if not os.path.exists(save_path):
+                    os.mkdir(save_path)
+                image = image.cpu().numpy() * 255
+                image = image.transpose(0,2,3,1).astype(int)
+
+                imgs = self.color_images(pred, target)
+                self.save_images(imgs, batch, save_path, False)
+                self.save_images(image, batch, save_path, True)
+                batch -= 1
+
         Acc = self.evaluator.Building_Acc()
         IoU = self.evaluator.Building_IoU()
         mIoU = self.evaluator.Mean_Intersection_over_Union()
         return Acc, IoU, mIoU
 
     def test(self):
-        A, I, Im = self.get_performance(self.source_loader)
+        A, I, Im = self.get_performance(self.source_loader, None, config.dataset)
         tA, tI, tIm = [], [], []
-        for dl in self.target_loader:
-            tA_, tI_, tIm_ = self.get_performance(dl)
+        for dl, tl, city in zip(self.target_loader, self.target_trainloader, self.target):
+            tA_, tI_, tIm_ = self.get_performance(dl, tl, city)
             tA.append(tA_)
             tI.append(tI_)
             tIm.append(tIm_)
@@ -83,20 +127,51 @@ class Test:
         for i, city in enumerate(self.target):
             print("{}: Acc:{}, IoU:{}, mIoU:{}".format(city, tA[i], tI[i], tIm[i]))
             res[city] = {'Acc': tA[i], 'IoU': tI[i], 'mIoU': tIm[i]}
-        with open('train_log/test.json', 'w') as f:
+
+        if self.bn:
+            name = 'train_log/test_bn.json'
+        else:
+            name = 'train_log/test.json'
+
+        with open(name, 'w') as f:
             json.dump(res, f)
 
+    def save_images(self, imgs, batch_index, save_path, if_original=False):
+        for i, img in enumerate(imgs):
+            img = img[:,:,::-1] # change to BGR
+            #from IPython import embed
+            #embed()
+            if not if_original:
+                cv2.imwrite(os.path.join(save_path, str(batch_index) + str(i) + '_Original.jpg'), img)
+            else:
+                cv2.imwrite(os.path.join(save_path, str(batch_index) + str(i) + '_Pred.jpg'), img)
 
-
+    def color_images(self, pred, target):
+        imgs = []
+        for p, t in zip(pred, target):
+            tmp = p * 2 + t
+            np.squeeze(tmp)
+            img = np.zeros((p.shape[0], p.shape[1], 3))
+            # bkg:negative, building:postive
+            #from IPython import embed
+            #embed()
+            img[np.where(tmp==0)] = [0, 0, 0] # Black RGB, for true negative
+            img[np.where(tmp==1)] = [255, 0, 0] # Red RGB, for false negative
+            img[np.where(tmp==2)] = [0, 255, 0] # Green RGB, for false positive
+            img[np.where(tmp==3)] = [255, 255, 0] #Yellow RGB, for true positive
+            imgs.append(img)
+        return imgs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('model', default='train_log/models/epoch1.pth')
     parser.add_argument('--cuda', default=True)
- 
+    parser.add_argument('--bn', default=True)
+    parser.add_argument('--save_path', default='train_log/test_images/')
+    parser.add_argument('--save_batch', default=0, type=int)
     args = parser.parse_args()
 
-    test = Test(args.model, config, args.cuda)
+    test = Test(args.model, config, args.bn, args.save_path, args.save_batch, args.cuda)
     test.test()
 
 
