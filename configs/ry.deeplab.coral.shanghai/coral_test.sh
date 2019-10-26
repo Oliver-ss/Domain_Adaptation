41b9b3f3a68296b2b1224453874a6acb4fa70ac9
Runyu He <rh254@cs.duke.edu>
2019-10-26 11:18:10 -0400

    Change script

---
+++ configs/ry.deeplab.coral.shanghai/coral_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH -o coral_neck_427_out
+#SBATCH -e coral_neck_427_err
+#SBATCH --gres=gpu:1
+
+python3 ~/Domain_Adaptation/configs/ry.deeplab.coral.shanghai/coral_test.py "/usr/xtmp/satellite/train_models/xh.deeplab.mobilenet.shanghai/epoch427.pth" ${BN} ${SAVE}
+BN=True
+SAVE=0
--- configs/ry.deeplab.coral.shanghai/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH -o test_427_out
-#SBATCH -e test_427_err
-#SBATCH --gres=gpu:1
-
-python3 ~/Domain_Adaptation/configs/ry.deeplab.coral.shanghai/test.py "/usr/xtmp/satellite/train_models/xh.deeplab.mobilenet.shanghai/epoch427.pth"
