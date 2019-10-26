aa4e7e4574719e3412b80322b63dc2b29f5e5524
Runyu He <rh254@cs.duke.edu>
2019-10-26 16:15:08 -0400

    modified batchsize

--- configs/ry.deeplab.coral.shanghai/coral_test.py
+++ configs/ry.deeplab.coral.shanghai/coral_test.py
@@ -42,7 +42,7 @@ def neck_coral(src_feat, tar_feat):
     for feat in src_feat:
         for i in range(len(feat)):
             src_layers[i].append(feat[i].view(-1))
-    for i in range(len(src_feat)):
+    for i in range(len(src_layers)):
         src_layers[i] = torch.stack(src_layers[i])
     tar_layers = []
     for _ in range(len(tar_feat[0])):
@@ -50,12 +50,12 @@ def neck_coral(src_feat, tar_feat):
     for feat in tar_feat:
         for i in range(len(feat)):
             tar_layers[i].append(feat[i].view(-1))
-    for i in range(len(tar_feat)):
+    for i in range(len(tar_layers)):
         tar_layers[i] = torch.stack(tar_layers[i])
-    tans_layer = []
+    trans_layer = []
     for Xs, Xt in zip(src_layers, tar_layers):
-        Xt = torch.tensor(CORAL(Xt.data.cpu().numpy(), Xs.data.cpu().numpy()))
-        trans_layer.append(Xt)
+        Xt_t = torch.tensor(CORAL(Xt.data.cpu().numpy(), Xs.data.cpu().numpy()))
+        trans_layer.append(Xt_t)
     for i in range(len(trans_layer)):
         for j in range(len(trans_layer[i])):
             ret[j][i] = trans_layer[i][j]
@@ -241,13 +241,13 @@ class Test:
         Measure performamce
         '''
         src_target, src_neck, src_low_feat, src_size = self.get_neck_feat(self.source_loader)
-        A, I, Im = self.neck_coral_performance(src_target, torch.unsqueeze(src_neck,dim=0), torch.unsqueeze(src_low_feat,dim=0), src_size)
+        A, I, Im = self.neck_coral_performance(torch.split(src_target, 100, dim=0), torch.split(src_neck, 100, dim=0), torch.split(src_low_feat,100, dim=0), src_size)
         print("On source domain:", A, I, Im)
         tA, tI, tIm = [], [], []
         for dl in self.target_loader:
             t_target, t_neck, t_low_feat, t_size = self.get_neck_feat(dl)
             t_neck = neck_coral(src_neck, t_neck)
-            curA, cur_I, cur_Im = self.neck_coral_performance(t_target, torch.unsqueeze(t_neck,dim=0), torch.unsqueeze(t_low_feat,dim=0), t_size)
+            curA, cur_I, cur_Im = self.neck_coral_performance(torch.split(t_target,100,dim=0), torch.split(t_neck,100,dim=0), torch.split(t_low_feat,100,dim=0), t_size)
             tA.append(cur_A)
             tI.append(cur_I)
             tIm.append(cur_Im)
