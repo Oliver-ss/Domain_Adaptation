72b952a4bef254a6ea8460efa091f3995bbe99bf
Xiaohan Wang <xw176@cs.duke.edu>
2019-11-20 18:27:21 -0500

    gt_discriminator

--- configs/xh.deeplab.mobilenet.paris.gt_discriminator/common.py
+++ configs/xh.deeplab.mobilenet.paris.gt_discriminator/common.py
@@ -12,21 +12,22 @@ class Config:
     #Data
     all_dataset = ['Shanghai', 'Vegas', 'Paris', 'Khartoum']
     dataset = 'Paris'
-    train_num_workers = 4
+    train_num_workers = 2
     val_num_workers = 2
     img_root = '/usr/xtmp/satellite/spacenet/'
     #Train
-    batch_size = 16
+    batch_size = 4
     freeze_bn = False
     sync_bn = False
     loss = 'ce' #['ce', 'focal']
-    epochs = 10000
+    epochs = 50000
     lr = 1e-3
     momentum = 0.9
     weight_decay = 5e-4
     lr_scheduler = 'cos'
     lr_step = 5
     warmup_epochs = 10
+    T = 100 # how many epoch in a period
 
 
 config = Config()
--- configs/xh.deeplab.mobilenet.paris.gt_discriminator/train.py
+++ configs/xh.deeplab.mobilenet.paris.gt_discriminator/train.py
@@ -11,9 +11,12 @@ from model.deeplab import *
 from utils.loss import SegmentationLosses
 from utils.lr_scheduler import LR_Scheduler
 from utils.metrics import Evaluator
+from utils.discriminator import FCDiscriminator
 import json
 import visdom
 import torch
+from torch.autograd import Variable
+from utils.argmax import ArgMax
 
 
 class Trainer(object):
@@ -23,6 +26,12 @@ class Trainer(object):
         self.vis = visdom.Visdom(env=os.getcwd().split('/')[-1])
         # Define Dataloader
         self.train_loader, self.val_loader, self.test_loader, self.nclass = make_data_loader(config)
+        self.gpu = args.gpu
+
+        # labels for adversarial training
+        self.gt_label = 0
+        self.prediction_label = 1
+        self.argmax = ArgMax()
 
         # Define network
         model = DeepLab(num_classes=self.nclass,
@@ -30,6 +39,8 @@ class Trainer(object):
                         output_stride=config.out_stride,
                         sync_bn=config.sync_bn,
                         freeze_bn=config.freeze_bn)
+        model_D = FCDiscriminator(num_classes=self.nclass)
+        model_D.train()
 
         train_params = [{'params': model.get_1x_lr_params(), 'lr': config.lr},
                         {'params': model.get_10x_lr_params(), 'lr': config.lr * 10}]
@@ -37,11 +48,15 @@ class Trainer(object):
         # Define Optimizer
         optimizer = torch.optim.SGD(train_params, momentum=config.momentum,
                                     weight_decay=config.weight_decay)
+        optimizer_D = torch.optim.Adam(model_D.parameters(), lr=1e-4, betas=(0.9, 0.99))
+        optimizer_D.zero_grad()
 
         # Define Criterion
         # whether to use class balanced weights
         self.criterion = SegmentationLosses(weight=None, cuda=args.cuda).build_loss(mode=config.loss)
+        self.criterion_D = torch.nn.BCEWithLogitsLoss()
         self.model, self.optimizer = model, optimizer
+        self.model_D, self.optimizer_D = model_D, optimizer_D
 
         # Define Evaluator
         self.evaluator = Evaluator(self.nclass)
@@ -52,10 +67,14 @@ class Trainer(object):
 
         # Using cuda
         if args.cuda:
-            self.model = torch.nn.DataParallel(self.model)
-            patch_replication_callback(self.model)
+            # self.model = torch.nn.DataParallel(self.model)
+            # patch_replication_callback(self.model)
+            # self.model_D = torch.nn.DataParallel(self.model)
+            # patch_replication_callback(self.model_D)
             # cudnn.benchmark = True
             self.model = self.model.cuda()
+            self.model_D = self.model_D.cuda()
+            self.argmax = self.argmax
 
         # Resuming checkpoint
         self.best_pred = 0.0
@@ -64,38 +83,99 @@ class Trainer(object):
                 raise RuntimeError("=> no checkpoint found at '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
             if args.cuda:
-                self.model.module.load_state_dict(checkpoint)
+                self.model.load_state_dict(checkpoint)
             else:
                 self.model.load_state_dict(checkpoint, map_location=torch.device('cpu'))
             print("=> loaded checkpoint '{}' (epoch {})"
                   .format(args.resume, args.start_epoch))
 
     def training(self, epoch):
-        train_loss = 0.0
-        self.model.train()
         tbar = tqdm(self.train_loader)
         num_img_tr = len(self.train_loader)
+
+        seg_loss = 0.0
+        adv_loss = 0.0
+        D_loss = 0.0
+
+        self.model.train()
+
         for i, sample in enumerate(tbar):
             iter = epoch * len(self.train_loader) + i
             self.vis.line(X=torch.tensor([iter]), Y=torch.tensor([self.optimizer.param_groups[0]['lr']]),
-                          win='lr', opts=dict(title='lr', xlabel='iter', ylabel='lr'),
-                          update='append' if iter>0 else None)
+                          win='lr_seg', opts=dict(title='lr', xlabel='iter', ylabel='lr'),
+                          update='append' if iter > 0 else None)
+            self.vis.line(X=torch.tensor([iter]), Y=torch.tensor([self.optimizer_D.param_groups[0]['lr']]),
+                          win='lr_adv', opts=dict(title='lr', xlabel='iter', ylabel='lr'),
+                          update='append' if iter > 0 else None)
+
             image, target = sample['image'], sample['label']
             if self.args.cuda:
                 image, target = image.cuda(), target.cuda()
+
             self.scheduler(self.optimizer, i, epoch, self.best_pred)
             self.optimizer.zero_grad()
+            self.optimizer_D.zero_grad()
+
+            # train the segmentation network
+            # don't accumulate grads in D
+            for param in self.model_D.parameters():
+                param.requires_grad = False
+
+            # seg loss
             output = self.model(image)
-            loss = self.criterion(output, target)
-            loss.backward()
+            loss1 = self.criterion(output, target)
+            loss1.backward()
             self.optimizer.step()
-            train_loss += loss.item()
-            tbar.set_description('Train loss: %.3f' % (train_loss / (i + 1)))
+            seg_loss += loss1.item()
+
+            # adv loss
+            print(self.argmax.apply(output))
+            D_out = self.model_D(self.argmax.apply(output))
+            loss2 = self.criterion_D(D_out,
+                                     Variable(torch.FloatTensor(D_out.data.size()).fill_(self.gt_label)).cuda(self.gpu))
+            loss2.backward()
+            self.optimizer_D.step()
+            adv_loss += loss2.item()
+
+            # train the discriminator
+            # bring back requires_grad
+            for param in self.model_D.parameters():
+                param.requires_grad = True
+
+            # train_with_prediction
+            output = output.detach()
+            D_out1 = self.model_D(self.argmax.apply(output))
+
+            loss_D1 = self.criterion_D(D_out1,
+                                       Variable(torch.FloatTensor(D_out1.data.size()).fill_(self.prediction_label)).cuda(
+                                           self.gpu))
+
+            loss_D1.backward()
+            D_loss += loss_D1.data.cpu().numpy()
+
+            # train with gt
+            D_out2 = self.model_D(target)
+            loss_D2 = self.criterion_D(D_out2,
+                                       Variable(torch.FloatTensor(D_out2.data.size()).fill_(self.gt_label)).cuda(
+                                           self.gpu))
+
+            loss_D2.backward()
+            D_loss += loss_D2.data.cpu().numpy()
+
+            tbar.set_description('[Train] Seg loss: %.3f, Adv loss: %.3f, D loss: %.3f' \
+                                 % (seg_loss / (i + 1), adv_loss / (i + 1), D_loss / (i + 1)))
 
         print('[Epoch: %d, numImages: %5d]' % (epoch, i * self.config.batch_size + image.data.shape[0]))
-        print('Loss: %.3f' % train_loss)
+        print('Seg loss: %.3f, Adv loss: %.3f, D loss: %.3f' \
+              % (seg_loss / (i + 1), adv_loss / (i + 1), D_loss / (i + 1)))
 
-        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([train_loss]), win='loss', name='train',
+        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([seg_loss]), win='seg_loss', name='train',
+                      opts=dict(title='loss', xlabel='epoch', ylabel='loss'),
+                      update='append' if epoch > 0 else None)
+        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([adv_loss]), win='adv_loss', name='train',
+                      opts=dict(title='loss', xlabel='epoch', ylabel='loss'),
+                      update='append' if epoch > 0 else None)
+        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([D_loss]), win='D_loss', name='train',
                       opts=dict(title='loss', xlabel='epoch', ylabel='loss'),
                       update='append' if epoch > 0 else None)
 
@@ -110,9 +190,14 @@ class Trainer(object):
                 image, target = image.cuda(), target.cuda()
             with torch.no_grad():
                 output = self.model(image)
-            loss = self.criterion(output, target)
-            test_loss += loss.item()
-            tbar.set_description('Test loss: %.3f' % (test_loss / (i + 1)))
+            seg_loss = self.criterion(output, target)
+            seg_loss += seg_loss.item()
+            output = output.detach()
+            D_out = self.model_D(self.argmax(output))
+            adv_loss = self.criterion_D(D_out,
+                                        Variable(torch.FloatTensor(D_out.data.size()).fill_(self.gt_label)).cuda(
+                                            self.gpu))
+            tbar.set_description('[Test] Seg loss: %.3f, Adv loss: %.3f' % (seg_loss / (i + 1), adv_loss / (i + 1)))
             pred = output.data.cpu().numpy()
             target = target.cpu().numpy()
             pred = np.argmax(pred, axis=1)
@@ -128,9 +213,11 @@ class Trainer(object):
         print('Validation:')
         print('[Epoch: %d, numImages: %5d]' % (epoch, i * self.config.batch_size + image.data.shape[0]))
         print("Acc:{}, IoU:{}, mIoU:{}".format(Acc, IoU, mIoU))
-        print('Loss: %.3f' % test_loss)
+        print('Seg loss: %.3f, Adv loss: %.3f' % (seg_loss / (i + 1), adv_loss / (i + 1)))
 
-        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([test_loss]), win='loss', name='val',
+        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([seg_loss]), win='seg_loss', name='val',
+                      update='append')
+        self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([adv_loss]), win='adv_loss', name='val',
                       update='append')
         self.vis.line(X=torch.tensor([epoch]), Y=torch.tensor([Acc]), win='metrics', name='acc',
                       opts=dict(title='metrics', xlabel='epoch', ylabel='performance'),
@@ -145,7 +232,7 @@ class Trainer(object):
             is_best = True
             self.best_pred = new_pred
             print('Saving state, epoch:', epoch)
-            torch.save(self.model.module.state_dict(), self.args.save_folder + 'models/'
+            torch.save(self.model.state_dict(), self.args.save_folder + 'models/'
                        + 'epoch' + str(epoch) + '.pth')
             loss_file = {'Acc': Acc, 'IoU': IoU, 'mIoU': mIoU}
             with open(os.path.join(self.args.save_folder, 'eval', 'epoch' + str(epoch) + '.json'), 'w') as f:
@@ -166,7 +253,8 @@ def main():
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     # checking point
-    parser.add_argument('--resume', type=str, default=None,
+    parser.add_argument('--resume', type=str,
+                        default=None,
                         help='put the path to resuming file if needed')
     parser.add_argument('--checkname', type=str, default=None)
     parser.add_argument('--save_folder', default='train_log/',
