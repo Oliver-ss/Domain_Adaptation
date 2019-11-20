d89806e2a6a366c4b9c8b615c175b858853ff1c7
Runyu He <rh254@cs.duke.edu>
2019-11-19 23:16:29 -0500

    Fixed typo

--- scripts/sample_test.sh
+++ scripts/sample_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH -o sample_test_out
-#SBATCH -e sample_test__err
-#SBATCH --gres=gpu:1
+#SBATCH -e sample_test_err
+#SBATCH --gres=gpu:3
 
 python3 ~/Domain_Adaptation/configs/ry.deeplab.coral.shanghai/coral_test.py "/usr/xtmp/satellite/train_models/xh.deeplab.mobilenet.shanghai/epoch427.pth" True 0
