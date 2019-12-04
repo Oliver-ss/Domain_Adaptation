bf8fc1d2f560abd77eefb5f4f43149ae2d26df45
Runyu He <rh254@cs.duke.edu>
2019-12-03 21:47:47 -0500

    Added 8-band Shanghai

---
+++ configs/ry.deeplab.MUL.shanghai/common.py
@@ -0,0 +1,33 @@
+'''*************************************************************************
+	> File Name: common.py
+	> Author: yuansong
+	> Mail: yuansongwx@outlook.com
+	> Created Time: Mon 21 Oct 2019 01:46:28 PM EDT
+ ************************************************************************'''
+class Config:
+    #Model
+    backbone = 'mobilenet' #backbone name ['resnet', 'xception', 'drn', 'mobilenet']
+    out_stride = 8 #network output stride
+
+    #Data
+    all_dataset = ['Shanghai', 'Vegas', 'Paris', 'Khartoum']
+    dataset = 'Shanghai'
+    train_num_workers = 4
+    val_num_workers = 2
+    img_root = '/usr/xtmp/satellite/spacenet_new/data/'
+    #Train
+    batch_size = 16
+    freeze_bn = False
+    sync_bn = False
+    loss = 'ce' #['ce', 'focal']
+    epochs = 600
+    lr = 1e-3
+    momentum = 0.9
+    weight_decay = 5e-4
+    lr_scheduler = 'cos'
+    lr_step = 5
+    warmup_epochs = 10
+    T = 100 # how many epoch in a period
+
+
+config = Config()
